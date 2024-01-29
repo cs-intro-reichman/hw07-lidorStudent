e8934ec65ebac55ec19e7c13bd88fdb8dfe0cb20
Lidor Amraby <lidoryosef.amraby@post.runi.ac.il>
2024-01-29 17:09:24 +0200

    all works

--- SpellChecker.java
+++ SpellChecker.java
@@ -20,6 +20,20 @@ public class SpellChecker {
 
 	public static int levenshtein(String word1, String word2) {
 		// Your code goes here
+		if (word2.length() == 0) {
+			return word1.length();
+		}
+		if (word1.length() == 0) {
+			return word2.length();
+		}
+		if (word1.charAt(0) == word2.charAt(0)) {
+			return levenshtein(tail(word1), tail(word2));
+		}
+		return 1 + Math.min(
+				   Math.min(
+					levenshtein(tail(word1), word2),
+					levenshtein(word1, tail(word2))),
+					levenshtein(tail(word1), tail(word2)));
 	}
 
 	public static String[] readDictionary(String fileName) {
@@ -37,6 +51,19 @@ public class SpellChecker {
 
 	public static String spellChecker(String word, int threshold, String[] dictionary) {
 		// Your code goes here
+		int length = dictionary.length;
+		int minDistance = levenshtein(word, dictionary[0]);
+		String similarWord = "";
+		for (int i = 0; i < length; i++) {
+			if (levenshtein(word, dictionary[i]) <= minDistance) {
+				minDistance = levenshtein(word, dictionary[i]);
+				similarWord = dictionary[i];
+			}
+		}
+		if (minDistance > threshold) {
+			return word;
+		}
+		return similarWord;
 	}
 
 }
